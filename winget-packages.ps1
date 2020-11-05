f7cdbbefe4d6bcc3726496d0aadc1ad688873391
KwangSeob Jeong <lesstif@gmail.com>
2020-11-05 13:33:59 +0900

    Update winget-packages.ps1

--- winget-packages.ps1
+++ winget-packages.ps1
@@ -1,3 +1,22 @@
-#
-winget install ultrasearch
+#### Util
+$UTIL="ultrasearch", "Total Commander" ## Everything
+
+foreach ($item in $UTIL) {
+  winget install $item
+}
+
 # winget install Everything
+
+#### Editor
+$EDITORS="vim", "Notepad++"
+
+foreach ($item in $EDITORS) {
+  winget install $item
+}
+
+#### devel
+$DEVELS= "Beyond Compare 4"
+
+foreach ($item in $DEVELS) {
+  winget install $item
+}
