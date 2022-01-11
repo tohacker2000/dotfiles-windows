b8085ab8d30e71d7f1936a32fe8ac68f83d0dccb
KwangSeob Jeong <lesstif@gmail.com>
2022-01-11 10:53:33 +0900

    Update winget-packages.ps1

--- winget-packages.ps1
+++ winget-packages.ps1
@@ -1,5 +1,5 @@
 #### Util
-$UTIL="ultrasearch", "Total Commander" ## Everything
+$UTIL="ultrasearch", "Total Commander", "Bandisoft.Honeyview", "voidtools.Everything"
 
 foreach ($item in $UTIL) {
   winget install $item
@@ -8,15 +8,29 @@ foreach ($item in $UTIL) {
 # winget install Everything
 
 #### Editor
-$EDITORS="vim", "Notepad++"
+$EDITORS="vim.vim", "Notepad++.Notepad++
 
 foreach ($item in $EDITORS) {
   winget install $item
 }
 
 #### devel
-$DEVELS= "Beyond Compare 4"
+$DEVELS= "ScooterSoftware.BeyondCompare4", "git.git", "Atlassian.Sourcetree"
 
 foreach ($item in $DEVELS) {
-  winget install $item
+  winget install $item -h
+}
+
+### PDF
+$PDFS = "Foxit.FoxitReader", "Adobe.Acrobat.Reader.32-bit"
+
+foreach ($item in $PDFS) {
+  winget install $item -h
+}
+
+### Jetbrains
+$JBS = "JetBrains.Toolbox", "JetBrains.WebStorm", "JetBrains.PHPStorm", "JetBrains.IntelliJIDEA.Ultimate", "JetBrains.DataGrip", "JetBrains.CLion"
+
+foreach ($item in $JBS) {
+  winget install $item -h
 }
